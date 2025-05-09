c8ccfefc6810f958ebddca9eb8e4210c2224a1d2
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-05-09 19:41:50 +0200

    Add CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
+import "./global.css";
+
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-
 export default function Page() {
-  return (
-    <h1>Hello World!</h1>
-  );
+    return (
+        <h1 className="text-3xl font-bold underline">Hello World!</h1>
+    );
 }

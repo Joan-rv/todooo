da7fd90833ce4b0a320869c11b954426911189eb
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-05-10 11:18:22 +0200

    Render list of Todos

--- components/todo-list.tsx
+++ components/todo-list.tsx
@@ -1,10 +1,21 @@
+"use client";
+
+import { useState } from "react";
 import TodoItem from "./todo-item"
 
 export default function TodoList() {
+    const [todos, setTodos] = useState([
+        { id: 1, title: "Some task", finished: false },
+        { id: 2, title: "Some task 2", finished: false },
+    ])
+
     return (
         <ul className="bg-gray-100 w-3/12 py-2 rounded-md flex flex-col items-stretch">
-            <li><TodoItem id={1} title="Some task" finished={false} /></li>
-            <li><TodoItem id={1} title="Some task 2" finished={false} /></li>
+            {todos.map((todo) => (
+                <li key={todo.id}>
+                    <TodoItem {...todo} />
+                </li>
+            ))}
         </ul>
     );
 }

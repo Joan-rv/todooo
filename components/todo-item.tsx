b7df5c8c1ffe21b593c280b44974ecf69fa00343
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-05-10 19:55:41 +0200

    Make whole list item clickable

--- components/todo-item.tsx
+++ components/todo-item.tsx
@@ -17,9 +17,16 @@ export default function TodoItem({ id, title, finished }: Todo) {
   }
 
   return (
-    <div className="flex bg-gray-200 rounded-md p-2 m-2">
+    <div
+      onClick={handleCheckbox}
+      className="flex bg-gray-200 rounded-md p-2 m-2 hover:bg-gray-300 transition"
+    >
       <p className="flex-1">{title}</p>
-      <input type="checkbox" checked={checked} onChange={handleCheckbox} />
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={(e) => e.stopPropagation()}
+      />
     </div>
   );
 }

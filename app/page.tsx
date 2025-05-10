5840f0f416f6393e241437cbd1a8805e7fda890c
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-05-10 21:05:35 +0200

    More descriptive tag

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import TodoList from "@/components/todo-list";
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center">
+    <main className="flex flex-col items-center">
       <h1 className="text-3xl font-bold py-2">todooo</h1>
       <TodoList />
-    </div>
+    </main>
   );
 }

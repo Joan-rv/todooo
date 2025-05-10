68ea2ec101b4551763a66bc5592881ea2dea1df9
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-05-10 12:08:34 +0200

    Start with no default todo items

--- components/todo-list.tsx
+++ components/todo-list.tsx
@@ -2,12 +2,10 @@
 
 import { useState } from "react";
 import TodoItem from "./todo-item"
+import { Todo } from "@/types/todo"
 
 export default function TodoList() {
-    const [todos, setTodos] = useState([
-        { id: 1, title: "Some task", finished: false },
-        { id: 2, title: "Some task 2", finished: false },
-    ]);
+    const [todos, setTodos] = useState<Todo[]>([]);
 
     const [newTodo, setNewTodo] = useState("");
 
@@ -23,13 +21,15 @@ export default function TodoList() {
                 <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} className="flex-1 border rounded" />
                 <button onClick={handleClick} className="ml-2 rounded bg-blue-500 text-white px-2 hover:bg-blue-600 transition" >Add todo item</button>
             </div>
-            <ul className="bg-gray-100 py-2 rounded-md flex flex-col items-stretch">
-                {todos.map((todo) => (
-                    <li key={todo.id}>
-                        <TodoItem {...todo} />
-                    </li>
-                ))}
-            </ul>
+            {todos.length == 0 ? "No todo items yet" :
+                <ul className="bg-gray-100 py-2 rounded-md flex flex-col items-stretch">
+                    {todos.map((todo) => (
+                        <li key={todo.id}>
+                            <TodoItem {...todo} />
+                        </li>
+                    ))}
+                </ul>
+            }
         </div>
     );
 }

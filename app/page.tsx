014a81f71034550e0c26d5c4afd8aa87679812b6
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-05-10 19:42:46 +0200

    Improve styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import TodoList from "@/components/todo-list";
 export default function Page() {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-3xl font-bold py-2">Todooo</h1>
+      <h1 className="text-3xl font-bold py-2">todooo</h1>
       <TodoList />
     </div>
   );
--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import { FaGithub } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="bg-blue-200 flex justify-center items-center py-4">
+    <footer className="bg-blue-600 text-white flex justify-center items-center py-4">
       <h1>Created by Joan Ripoll</h1>
       <Link className="mx-4" href="https://github.com/Joan-rv">
         <FaGithub size={25} />

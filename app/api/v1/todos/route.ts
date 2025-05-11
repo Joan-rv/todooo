547193bd82d0d2aefe036d1e029905ab8e217675
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-05-11 19:32:39 +0200

    User specific todo items

--- app/api/v1/todos/route.ts
+++ app/api/v1/todos/route.ts
@@ -1,11 +1,23 @@
+import { cookies } from "next/headers";
 import { Todo } from "@/types/todo";
 import { sql } from "@/lib/db";
 import { apiError } from "@/lib/api";
+import { validateToken } from "@/lib/jwt";
 
 export async function GET() {
+  const tokenCookie = (await cookies()).get("token");
+  if (tokenCookie === undefined) {
+    return apiError("Not logged in", 403);
+  }
+  const token = tokenCookie.value;
+  const user = validateToken(token);
+  if (user === null) {
+    return apiError("Invalid session, relogin", 403);
+  }
+
   const todos = await sql<
     Todo[]
-  >`select * from todos order by created_at desc;`;
+  >`select * from todos where user_id=${user.id} order by created_at desc;`;
 
   return new Response(JSON.stringify(todos), {
     status: 200,
@@ -15,12 +27,23 @@ export async function GET() {
 
 export async function POST(request: Request) {
   const { title } = await request.json();
+
+  const tokenCookie = (await cookies()).get("token");
+  if (tokenCookie === undefined) {
+    return apiError("Not logged in", 403);
+  }
+  const token = tokenCookie.value;
+  const user = validateToken(token);
+  if (user === null) {
+    return apiError("Invalid session, relogin", 403);
+  }
+
   if (title === undefined) {
     return apiError("Missing title", 400);
   }
 
   const [newTodo] =
-    await sql`INSERT INTO todos (title) VALUES (${title}) RETURNING *`;
+    await sql`insert into todos (title,user_id) values (${title},${user.id}) returning *`;
   if (newTodo === undefined) {
     return apiError("Failed to create new todo", 500);
   }
